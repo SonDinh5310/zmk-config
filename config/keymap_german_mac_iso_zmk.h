40958d2a1de0ace28838bbcd2169b3e5c83f21e8
Son Dinh <40727116+SonDinh5310@users.noreply.github.com>
2025-06-15 17:00:03 +0700

    Make split keyboards work with prospector dongle (#3)
    * Remove unused configs
    
    * Update kconfig.defconfig & dongle config
    
    * Update kconfig.defconfig, dongle config & sofle.zmk.yml
    
    * Update kconfig.defconfig
    
    * Update config
    
    * Update sofle config
    
    * Update sofle.keymap
    
    * Update sofle.keymap
    
    * Update sofle configs
    
    * Update sofle configs
    
    * Update sofle configs
    
    * Update build config
    
    * Update dongle overlya
    
    * Update dongle overlay
    
    * Update dongle overlay
    
    * Update sofle.dtsi
    
    * Update sofle.dtsi
    
    * Update sofle.dtsi
    
    * Update sofle.dtsi
    
    * Update sofle configs

--- config/keymap_german_mac_iso_zmk.h
+++ config/keymap_german_mac_iso_zmk.h
@@ -1,38 +1,3 @@
-/* ========================================
-FileName: keymap_german_mac_iso_zmk.h
-Date: 15:17 23.September.2022
-Author: Marcos Chow Castro
-Email: mctechnology170318@gmail.com
-GitHub: https://github.com/mctechnology17
-Brief: define keymap for the wireless corne keyboard
-Use: in your keymap file, include this file
-     #include "keymap_german_mac_iso_zmk.h"
-===========================================
-Copyright 2022 Marcos Ivan Chow Castro
-Inspired by Stephen Bösebeck
-                           ╔═╦═╦═╗
-                    ╔════╗ ║║║║║╔╝
-                    ║╔╗╔╗║ ║║║║║╚╗
-                    ╚╝║║╚╝ ║╠═╩╩═╝
-                      ║╠═╦═╣╚╦═╦╦═╦╗╔═╦═╦╦╗
-                      ║║╩╣═╣║║║║║╬║╚╣╬║╬║║║
-                      ╚╩═╩═╩╩╩╩═╩═╩═╩═╬╗╠╗║
-                                      ╚═╩═╝
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 // #pragma once
 
 #define XXX &none
